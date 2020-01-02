efee3200c4bf29aee7680a574e33a7b4cb937416
Sam Beran <sam.beran@telepharm.com>
2020-01-02 15:40:57 -0600

    type keyword

--- tjs.ts
+++ tjs.ts
@@ -5,10 +5,24 @@ export type AnyJsonObject = {[key: string]: AnyJson }
 export type AnyJsonArray = AnyJson[]
 export type AnyJson = AnyJsonPrimitive | AnyJsonObject | AnyJsonArray
 
-type JsonSchema = 'string' | {}
 
-export type TypeOf<JsonSchema> = 
-  JsonSchema extends 'string' ? string : AnyJsonObject
+type JsonTypes = {
+  string: string
+  number: number
+}
+
+interface JsonSchema {
+  type?: keyof JsonTypes 
+}
+
+type TypeKeyword<S extends keyof JsonTypes> = { type: S }
+
+type JsonSchemaInput = JsonSchema | keyof JsonTypes
+
+export type TypeOf<S extends JsonSchemaInput> = 
+    S extends keyof JsonTypes ? JsonTypes[S]
+  : S extends TypeKeyword<infer T> ? JsonTypes[T]
+  : AnyJson
 
 
-export function validate<S extends JsonSchema>(schema: S): TypeOf<S> { throw 'nope'}
+export function validate<S extends JsonSchemaInput>(schema: S): TypeOf<S> { throw 'nope'}
