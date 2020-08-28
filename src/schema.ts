b9186a5350684959779899250a1de4ffc1b6387d
Sam Beran <sberan@gmail.com>
2020-08-28 14:15:37 -0500

    json: add ability to require object properties

--- src/schema.ts
+++ src/schema.ts
@@ -6,16 +6,28 @@ export type AnyJsonObject = {[key: string]: AnyJsonValue}
 export type AnyJsonArray = AnyJson[]
 export type AnyJson = AnyJsonPrimitive | AnyJsonObject | AnyJsonArray
 
-interface JSONObjectType {
+interface JSONObjectSpec {
   properties?: {[key:string] : AnyJson}
+  required?: string
 }
 
-//TODO can we make this just JSONObject<{}> for consumers?
-export type JsonObject<T extends JSONObjectType> = {
-  [key:string]: AnyJsonValue
-} & ('properties' extends keyof T ? Partial<T['properties']> : {})
+type DefinedProperties<T extends JSONObjectSpec> =
+  'properties' extends keyof T ? NonNullable<T['properties']> : {}
 
-export type JSONTypes<ObjectType extends JSONObjectType> = {
+type RequiredUnknownKeys<T extends JSONObjectSpec> =
+  Exclude<RequiredKeys<T>, keyof DefinedProperties<T>>
+
+type RequiredKeys<T extends JSONObjectSpec> =
+  'required' extends keyof T ? NonNullable<T['required']> : never
+
+//TODO can we make this just JSONObject<{}> for consumers?
+export type JsonObject<T extends JSONObjectSpec> = 
+  { [key:string]: AnyJsonValue }
+  & Object.Pick<DefinedProperties<T>, RequiredKeys<T>>
+  & Omit<Partial<DefinedProperties<T>>, RequiredKeys<T>>
+  & {[P in RequiredUnknownKeys<T>]: AnyJson}
+
+export type JSONTypes<ObjectType extends JSONObjectSpec> = {
   'null': null
   'string': string
   'number': number
--- test/json.typedefs.ts
+++ test/json.typedefs.ts
@@ -9,3 +9,11 @@ let someObj: JsonObject<{ properties: { a: 24 }}> = { a: 24 }
 someObj.a // $ExpectType 24 | undefined
 someObj.b // $ExpectType AnyJsonValue
 
+
+let someObjRequired: JsonObject<{properties: {a: 24, b:32, c: 'asdf' }, required: 'a' | 'b' | 'd'}> = {a: 24, b: 32, d: ['a']}
+someObjRequired.a // $ExpectType 24
+someObjRequired.b // $ExpectType 32
+someObjRequired.c // $ExpectType "asdf" | undefined
+someObjRequired.d // $ExpectType AnyJson
+someObjRequired.e // $ExpectType AnyJsonValue
+
