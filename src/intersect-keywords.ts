d70720de6d435cd88a1c5ad094a799d93a28c5cd
Sam Beran <sberan@gmail.com>
2020-09-30 20:38:25 -0500

    object properties

--- src/intersect-keywords.ts
+++ src/intersect-keywords.ts
@@ -6,16 +6,15 @@ export type Keywords = Partial<
   TypeKeyword<JSONTypeName>
   & ConstKeyword<AnyJson>
   & EnumKeyword<AnyJson>
-  & PropertiesKeyword<{[key: string]: Keywords}>
+  & PropertiesKeyword<{ [key: string]: Keywords }>
   & RequiredKeyword<string>
 > & {
   items?: Keywords //TODO: tuple type (with tests). can't put this in the partial because circular refs
-  additionalProperties?: {true?: true, false?: false, keywords?: Keywords }
-  anyOf?: Keywords
+  additionalProperties?: { true?: true, false?: false, keywords?: Keywords }
 } 
 
 type TypeKeyword<TypeName extends string> =
-  { type: {[P in TypeName]?: true} }
+  { type: { [P in TypeName]?: true } }
 
 type ConstKeyword<Const extends AnyJson> =
   { const: Const }
@@ -27,37 +26,34 @@ type PropertiesKeyword<Properties extends {[key: string]: Keywords }> =
   { properties: Properties }
 
 type RequiredKeyword<Required extends string> =
-  { required: {[P in Required]: true} }
-
+  { required: { [P in Required]: true } }
 
 
 type JsonObjectSpec<T extends Keywords> =
   Extract<keyof T, 'properties' | 'additionalProperties' | 'required'> extends never
   ? AnyJsonObject
   : T extends PropertiesKeyword<infer Properties>
-    ? {[P in keyof Properties]: TypeOf<Properties[P]> }
-    : {}
-
-type NodeEntries<K> = {[P in keyof K]: {[P2 in P]: K[P2]}}[keyof K]
+    ? { [P in keyof Properties]: TypeOf<Properties[P]> }
+    : { }
 
 export type TypeOf<K extends Keywords> = K extends infer Entry
   ? Entry extends ConstKeyword<infer Const>
     ? Const
-    : Entry extends { type: infer TypeValues }
-      ? NodeEntries<TypeValues> extends infer Type
-        ? Type extends { string: true } ? string
-        : Type extends { boolean: true } ? boolean
-        : Type extends { number: true } ? number
-        : Type extends { null: true } ? null
-        : Type extends { object: true } ? JsonObjectSpec<K>
-        : Type extends { array: true } ? AnyJsonArray
+    : Entry extends TypeKeyword<infer TypeName>
+      ? TypeName extends string
+        ? 'string' extends TypeName ? string
+        : 'boolean' extends TypeName ? boolean
+        : 'number' extends TypeName ? number
+        : 'null' extends TypeName ? null
+        : 'object' extends TypeName ? JsonObjectSpec<K>
+        : 'array' extends TypeName ? AnyJsonArray
         : never
       : never
     : never
   : never
 
 export type AllOf<K extends Keywords> =
-  UnionToIntersection<K> extends { 'anyOf': Keywords}
+  UnionToIntersection<K> extends { 'anyOf': Keywords }
     ? UnionToIntersection<K>['anyOf'] extends infer I ? I extends Keywords ? {[P in keyof I]: I[P]} : never : never
     : never
 
